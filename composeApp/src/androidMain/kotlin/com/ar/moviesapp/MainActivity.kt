ae7723c960cdb514dd45ca74622ed60563a66c35
Md Asfakur Rahat <asfakur.rahat@brainstation-23.com>
2024-10-17 18:45:44 +0600

    Set Status bar to light because App is Dark

--- composeApp/src/androidMain/kotlin/com/ar/moviesapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ar/moviesapp/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.ar.moviesapp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                0,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        }
         setContent {
             App()
         }
